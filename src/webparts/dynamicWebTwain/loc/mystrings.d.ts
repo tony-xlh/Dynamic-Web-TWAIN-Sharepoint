066487836bbb1db3b564c0b5faf797e2ffccc6c3
tony-xlh <tony@dynamsoft.com>
2026-04-28 11:24:15 +0800

    init

---
+++ src/webparts/dynamicWebTwain/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IDynamicWebTwainWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'DynamicWebTwainWebPartStrings' {
+  const strings: IDynamicWebTwainWebPartStrings;
+  export = strings;
+}
